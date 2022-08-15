c7d7b10b136102d4e507b93b4aaa1851ce03c49d
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2022-08-15 22:22:46 +0900

    remove

--- design/layouts/page/presentational.stories.tsx
+++ design/layouts/page/presentational.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { Page } from "./index";
-
+import { Main } from "~design/layouts";
 import { Storybook } from "~shared/modules";
 import { screen } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
@@ -12,10 +12,17 @@ export default {
 } as Storybook.StoryMeta<typeof Page>;
 
 const Template: Storybook.StoryType<typeof Page> = {
-  render: (args) => <Page {...args} />,
+  render: (args) => (
+    <Main>
+      <Page {...args} />
+    </Main>
+  ),
 };
 
 export const Primary: Storybook.StoryType<typeof Page> = {
   ...Template,
   name: "Primary",
+  args: {
+    title: "page title",
+  },
 };
--- pages/index.stories.tsx
+++ pages/index.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import Page from "./index.page";
+import { Main } from "~design/layouts";
 
 import { Storybook } from "~shared/modules";
 import { screen } from "@storybook/testing-library";
@@ -12,7 +13,11 @@ export default {
 } as Storybook.StoryMeta<typeof Page>;
 
 const Template: Storybook.StoryType<typeof Page> = {
-  render: (args) => <Page {...args} />,
+  render: (args) => (
+    <Main>
+      <Page {...args} />
+    </Main>
+  ),
 };
 
 export const Primary: Storybook.StoryType<typeof Page> = {
